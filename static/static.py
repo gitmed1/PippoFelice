d3c01f9f7c8858372f0aabd76baf13fb2863758d
gitmed1 <160527471+gitmed1@users.noreply.github.com>
2026-05-19 10:54:04 +0200

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,6 +1,213 @@
 # static.py
 
 HTML = """
-
-questo contenuto è personalizzato!
+I’m also too poor to afford a real office.
+China is twenty times bigger than Japan.
+She never should have gone to that bar.
+She loved the overnight train from Prague to Krakow.
+He changed his favorite color by the day.
+She froze at the sight of the big spider.
+Laken bought a computer at thirty percent off the list price.
+This is my younger brother.
+I am dreaming of a better world.
+He will blame you for the accident.
+What was your contribution?
+I put on my underclothes, shirt and trousers.
+Is the room big enough for you?
+And I took the road less traveled by, just to be a hipster.
+I just walked on the furriest lawn.
+I especially appreciate how helpful and kind you’ve been throughout this entire process.
+I have no doubt that you’re going to nail this interview.
+I'm not sure if there is a meaning to life.
+I have lived in Shanghai for a long time.
+I need to go back to school.
+He didn't understand why anyone would pay a quarter of a million dollars to get a degree in singing.
+The plane ride to the mountains scared Kaxiang so much that he gave up on looking for the treasure and went to the bar instead.
+Your mom is so nice for giving me a ride home today.
+Can I help you?
+She still loves him.
+I said, "I don't want to be a pop star, I just want to be a regular teen."
+Allergies made him sneeze a lot.
+George brushes his teeth twice a day.
+The smoke was high in the sky.
+A wet road is usually slippery.
+The antique store was not like any antique store she had seen before in her life.
+You can make ice cream.
+I leave my house to go to the school.
+People don't realize how often you can fail as a writer, even after you get a good job or get a book deal.
+What do you need it for?
+Learning about other countries history is fun.
+There was a big gold star on the door.
+The baby was so cute but she was crying so loud I had to plug my ears.
+I think she likes me.
+That is a huge deal for me.
+Wearing bows was her favorite thing to do.
+I am dirt poor.
+We've got a major job ahead of us.
+The feelings on my side were real.
+The last city I wanted to visit was Boston.
+I am ready for the next step.
+I run four miles every morning.
+All this is due to our change in teaching methods.
+Money is no more than our servant.
+He continued talking.
+This could become a problem.
+I stepped on a corn flake.
+This is obviously just a big misunderstanding.
+She made him a cake for his party.
+I'm not sure if there is a meaning to life.
+Tom forgot his shopping list and only got half of what he wanted to.
+There used to be a pine tree in front of my house.
+I have a bunch of things I need to do.
+Her grandmother gave her the bracelet.
+He will blame you for the accident.
+She wore a beautiful dress.
+He is a smart boy.
+The Soviet Union: goes from a backwards agrarian society to the Space Age in 50 years, while almost single-handedly defeating the Nazis.
+She had a grudge against GrubHub.
+Most big Japanese firms depend on exports.
+I am the biggest Ariana Grande fan on the planet.
+Captain America is my favorite Avenger.
+The new one's twice as big as the old one.
+There's nobody out there.
+It doesn't matter what college you went to.
+Spiderman and Iron Man are two great men.
+Lenora only had eight fingers, after losing both of her pinkies to a freak accident with a hay baler.
+What's your favorite holiday?
+I went to California with my mom for a weekend girl's trip.
+I had just gotten the face where I wanted it when my classmate bumped into me and the clay fell over.
+They rode their bikes to the beach.
+The big building was blazing with lights.
+I don't want to live in a mansion.
+Oysters are disgusting.
+That's typically how it goes now.
+Cows eat grass.
+Do you like spaghetti?
+Your first week of yoga classes is on us.
+Do you eat ice cream?
+The view is breathtaking.
+Come on down!
+He carried the bananas in a cardboard box on his head.
+He stood no chance against her in a fight.
+I am hungry.
+Can I offer a suggestion?
+She gave him a wide grin.
+She read Little House on the Prairie as a child and decided to pretend like she did not have running water.
+I was not in a good mood on Friday.
+He wanted to find out what had happened to his brother.
+You will soon be used to living in a big city.
+I’ll buy you a drink.
+She is my younger sister.
+I spread the map on the table.
+It’s as flat as a pancake.
+The sea sparkled in the sunlight.
+You look just like your brother.
+The store had multiple skeletons they claimed were real, alongside a taxidermies, two-headed calf.
+She grinned with delight at the sight of her ice cream cone.
+She wanted to talk to her mom, but her mom was busy.
+She wasn't sure where all the lesbian bars had gone.
+This next part is crucial and requires deep concentration.
+My German vocabulary list is up to two thousand words now.
+Dozens of houses were burned down in that big fire.
+Tom is wearing clothes that are too big for him.
+My shoes tie up in a bow.
+It’s as quick as lightning.
+She was always worried that everyone secretly hated her.
+You shouldn't go swimming after eating a big meal.
+It's an awfully important job.
+You're simply the best.
+You should run to the party.
+I made a list of people I wanted to invite to my party.
+It is none of your business.
+We have a bigger problem than we thought.
+The guy I met online is probably a psychopath.
+She always wanted to live in a house that had big windows and ivy growing on the brick walls.
+Juan watched TV.
+His eyes were glazed over.
+My summer internship with them remains one of my most formative experiences.
+When you are a teenager you read Nietzsche.
+She attended kindergarten in 2013.
+Nothing hurts like the truth.
+She disliked her husband.
+That club is way too loud.
+There is not much to do in South Carolina other than sit on a beach.
+You don't have to pay rent for two years.
+That's the big question, isn't it?
+She meant to go to Gaborone, but mixed up Botswana and Zimbabwe and ended up in Harare instead.
+We have bigger problems to worry about.
+I can't believe you went skydiving last week.
+He is respectful to his elders.
+He's not the sharpest knife in the drawer.
+I fear that he drinks too much.
+He does laundry in his basement.
+She was twenty-two years old and had never drank a Coca Cola in her life.
+Going shopping can be expensive.
+Airplanes are, statistically, the safest form of transportation.
+I would like to make toast.
+He has made a big improvement in tennis.
+What did you dress up as last Halloween?
+She ripped a big hole in the back of her jeans and didn't notice for hours.
+It is one of the biggest summer music festivals.
+Are you going to have a blue birthday cake for your next birthday?
+I'm going to do you a favor.
+She sunburned herself so badly she looked like a tomato.
+Breaking glass can make a loud noise.
+That is a pencil.
+I'll make a statement.
+The price just covers the pre-tax cost.
+Books are hard to get into.
+You have a beautiful home.
+What do you like to do with other people?
+That movie franchise sucks.
+Did you hear about the new animal discovered in Ecuador?
+They are cruel to the animals.
+Tom has big plans.
+Are you cold?
+She was out on the deck of the ship when she saw the first ghosts.
+The book about women's rights was so profound it made her cry.
+There will be a big earthquake in the future.
+Oil is smelly.
+Maybe he was brokering a deal.
+Honesty is the best policy.
+Tom took a gulp of his drink.
+She was born to dance.
+She jumped in the lake and immediately choked on a wave of gross-tasting water.
+She spread the bread with socks.
+Criticizing racists isn't a repression of free speech, it is free speech.
+You should think about that for the rest of your life.
+We can go to Paris.
+Soccer is the devil's sport.
+We have weight training classes.
+What time is the pizza supposed to be here?
+In the near future, we may have a big earthquake in Japan.
+She washed it.
+Can you give me a minute?
+Actions speak louder than words.
+She washed the grapes.
+She thought the floor space could have been better used for extra seating.
+Bigger is not always better.
+When I was young, I played the guitar.
+The sponge absorbed the water quicker than the towel.
+Bigger doesn't always mean better.
+He has a very interesting looking face.
+She had long, yellow fingernails.
+They were afraid of the black dog.
+that's not even how to kill me.
+She started wondering if he was cheating on her when she saw the receipt for an expensive jewelry purchase.
+There were four people playing the game: her, her boyfriend, her boyfriend's roommate, Steve, and Steve's girlfriend, Cara.
+The man at the coffeeshop had been sitting there alone for some time before he was joined by a very beautiful woman.
+He was wearing a green fur coat.
+I’d like to honor an event.
+Where are you going in such a hurry?
+I miss my college friends.
+We put you on probation.
+Did she think she was going to swim all the way to Europe?
+It's the biggest sports shop in the region.
+The Ariake Sea is one of the biggest tidelands in Japan.
+I have an ever growing list of to-do items.
+She is very responsible.
+Are you honestly saying you believe in ghosts?
+She didn't realize there was a giant hole in her pants until after her date.
+I got my finger stuck in the door when I slammed it.
+She told him to bring a couple flashlights when they met up.
 """
